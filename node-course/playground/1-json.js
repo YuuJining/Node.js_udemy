d8658ca6fd2f26cfb321fbe1e71f546cc7a7faeb
Yujin Lee <yujin092444@gmail.com>
2020-09-06 15:37:48 +0900

    notes-app(store user data with json)

--- node-course/playground/1-json.js
+++ node-course/playground/1-json.js
@@ -1,9 +1,25 @@
 const fs = require('fs')
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
 
-const bookJSON = JSON.stringify(book)
+// const bookJSON = JSON.stringify(book)
 
-fs.writeFileSync('1-json.json', bookJSON)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+
+data.name = 'Yujin'
+data.age = 23
+
+const userJSON = JSON.stringify(data)
+fs.writeFileSync('1-json.json',userJSON)
