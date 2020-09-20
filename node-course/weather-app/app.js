172c717ec46d88677fc97afb2377dab726bbbd95
Yujin Lee <yujin092444@gmail.com>
2020-09-20 23:22:42 +0900

    weather-app(customizing http request)

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=9e8d5698a19a64a75629cb8360bebb71&query=37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0]+' It is currently '+response.body.current.temperature+' degrees out. It feels like ' +response.body.current.feelslike + ' degrees out.')
 })
