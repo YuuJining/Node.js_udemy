5d9ba0d5ed419c2f7bb09b6eac3e85b17e2af50f
Yujin Lee <yujin092444@gmail.com>
2020-09-26 21:34:29 +0900

    weather-app(callback chaining)

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,12 +1,26 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2]
 
-geocode('Boston', (error, data) => {
-    console.log('Error: ', error)
-    console.log('Data: ' , data)
-})
+if(!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if(error) {
+            return console.log(error)
+        } else {
+    
+        }
+    
+        forecast(data.latitude, data.longtitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
 
-forecast(-71.0596, 42.3605, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+
--- node-course/weather-app/utils/forecast.js
+++ node-course/weather-app/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longtitude, callback)=> {
-    const url = 'http://api.weatherstack.com/current?access_key=9e8d5698a19a64a75629cb8360bebb71&query='+ longtitude+ ',' +latitude
+    const url = 'http://api.weatherstack.com/current?access_key=9e8d5698a19a64a75629cb8360bebb71&query='+ latitude+ ',' +longtitude
 
     request({url: url, json: true}, (error, response)=> {
         if(error) {
@@ -9,11 +9,12 @@ const forecast = (latitude, longtitude, callback)=> {
         } else if(response.body.error) {
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined, {
-                description: response.body.current.weather_descriptions[0],
-                temperature: response.body.current.temperature,
-                feelslike: response.body.current.feelslike
-            })
+            callback(undefined, 
+                response.body.current.weather_descriptions[0]+' throughout the day. It is currently '+response.body.current.temperature+' degrees out. It feels like '+response.body.current.feelslike+' degrees out.'
+                // description: response.body.current.weather_descriptions[0],
+                // temperature: response.body.current.temperature,
+                // feelslike: response.body.current.feelslike
+            )
         }
     })
 }
