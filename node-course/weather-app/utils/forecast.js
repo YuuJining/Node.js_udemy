0217b5d47d21cb6b08f02d01b9b8f51d03feb1dd
Yujin Lee <yujin092444@gmail.com>
2020-10-02 00:06:10 +0900

    weather-app(destructing, property shorthand)

--- node-course/weather-app/utils/forecast.js
+++ node-course/weather-app/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request')
 const forecast = (latitude, longtitude, callback)=> {
     const url = 'http://api.weatherstack.com/current?access_key=9e8d5698a19a64a75629cb8360bebb71&query='+ latitude+ ',' +longtitude
 
-    request({url: url, json: true}, (error, response)=> {
+    request({url, json: true}, (error, { body })=> {
         if(error) {
             callback('Unable to connect to weather service', undefined)
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location',undefined)
         } else {
             callback(undefined, 
-                response.body.current.weather_descriptions[0]+' throughout the day. It is currently '+response.body.current.temperature+' degrees out. It feels like '+response.body.current.feelslike+' degrees out.'
+                body.current.weather_descriptions[0]+' throughout the day. It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.'
                 // description: response.body.current.weather_descriptions[0],
                 // temperature: response.body.current.temperature,
                 // feelslike: response.body.current.feelslike
