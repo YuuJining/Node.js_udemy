5bba64a4dd0822e48f8fb1980550f6d36e87d118
Yujin Lee <yujin092444@gmail.com>
2020-10-01 23:43:04 +0900

    weather-app(ES6 : Object Property Shorthand and Destructing)

---
+++ node-course/playground/5-es6-object.js
@@ -0,0 +1,34 @@
+const name = 'Yujin'
+const userAge = 23
+
+const user = {
+    name,
+    userAge,
+    location: 'Incheon'
+}
+
+console.log(user)
+
+// Object destructuring
+
+const product={
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4.2
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label:productLabel, stock, rating=5} = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, { label, stock}) => {
+    console.log(type, label, stock)
+} 
+
+transaction('order',product)
