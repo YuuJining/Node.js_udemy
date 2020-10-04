2994ca4c86704bd053e13fe2bc25a4e5aaf4d3ae
Yujin Lee <yujin092444@gmail.com>
2020-10-04 21:44:01 +0900

    weather-app(dynamic pages with templating - hbs)

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -5,7 +5,28 @@ const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Yujin Lee'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Yujin Lee'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
