679a0499b9df5c98fd98fc597c04965a171f079e
Yujin Lee <yujin092444@gmail.com>
2020-10-06 00:13:17 +0900

    weather-app(advanced templating - 보완 필요)

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,15 +1,19 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
 app.use(express.static(publicDirectoryPath))
 
 // Setup static directory to serve
